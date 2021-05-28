b10a271dfc6c1cffd2d8c4548624c9e28943057e
PorshnievN-IX <dporshniev@n-ix.com>
2021-05-28 15:51:26 +0300

    hat done

--- src/domain/Profile/ProfilePhoto.js
+++ src/domain/Profile/ProfilePhoto.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import PersonalPhoto from './images/photo.png';
+import { useSelector } from 'react-redux';
+import PersonalPhotoBlack from './images/photo-black.png';
+import PersonalPhotoPurple from './images/photo-purple.png';
+import PersonalPhotoWhite from './images/photo-white.png';
 import CartmanPhoto from './images/cartman.webp';
+import { selectPreset } from '../Settings/StageController.slice';
 
-const ProfilePhoto = () => (
-  <div className="profile-photo">
-    <div className="image-wrapper">
-      <img alt="Profile" src={PersonalPhoto} className="image-personal" />
-      <img alt="Mr Homer" src={CartmanPhoto} className="image-cartoon" />
+const ProfilePhoto = () => {
+  const selectTheme = useSelector(selectPreset);
+
+  const currentPhoto = {
+    default: PersonalPhotoPurple,
+    dark: PersonalPhotoBlack,
+    white: PersonalPhotoWhite,
+  };
+  return (
+    <div className="profile-photo">
+      <div className="image-wrapper">
+        <img alt="Profile" src={currentPhoto[selectTheme.theme]} className="image-personal" />
+        <img alt="Mr Homer" src={CartmanPhoto} className="image-cartoon" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProfilePhoto;
