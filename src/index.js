3138a3ed1bcd0c20f57430125b993696f14a9ff3
likozGO <likoz98@gmail.com>
2023-02-16 01:12:41 +0200

    minified images + improved pj file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Scrollbars } from 'react-custom-scrollbars-2';
 import store from './store';
 import './internationalization';
 import App from './app';
@@ -19,14 +18,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persist}>
-        <Scrollbars
-          style={{ display: 'flex' }}
-          autoHide
-          autoHideTimeout={1000}
-          autoHideDuration={200}
-        >
-          <App />
-        </Scrollbars>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
